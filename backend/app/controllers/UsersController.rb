81a5a2a734c282ec5941e26295b4c9c5ecd20bcc
Dave Roberts <dave.a.roberts@gmail.com>
2017-07-03 17:13:30 -0400

    WIP

--- backend/app/controllers/UsersController.rb
+++ backend/app/controllers/UsersController.rb
@@ -1,11 +1,29 @@
 require "roda"
+require 'json'
 
 class UsersController < Roda
+  users = [
+    {id: 1, name: "Dave", age: 34},
+    {id: 2, name: "Jen", age: 35}
+  ]
+
   plugin :json
   route do |r|
     response['Content-Type'] = 'application/json; charset=utf-8'
-    r.is do
-      ["Разходка в Докторската градина и наоколо", "list of users! Changed"]
+    r.get do
+      users
+    end
+    r.get Integer do |id|
+      users.find{|u|u[:id]==id}
+    end
+    r.post do
+      begin
+        body = r.body.read
+        json = JSON.parse(body)
+      rescue JSON::ParserError => e
+        puts e.class
+      end
+      json
     end
   end
 end
